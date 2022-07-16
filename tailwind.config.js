abe265990773cb9c37fe93f7e998311cc3668c99
Rayhan <rayhanprima99@gmail.com>
2022-07-16 13:12:46 +0700

    chore (styles): update tailwind default font, add tailwind utils, add material colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+const materialColor = require("./src/utils/material.colors");
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "class",
   theme: {
     extend: {
+      colors: materialColor.COLORS,
+      fontFamily: {
+        primary: ["Google Sans Display", "sans-serif", "Roboto"],
+        secondary: ["Plus Jakarta Sans", "sans-serif", "Roboto"],
+      },
       container: {
         center: true,
         padding: "1rem",
@@ -12,4 +18,3 @@ module.exports = {
   },
   plugins: [],
 };
-
