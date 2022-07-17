42d663adca6c30e1b84e94ace52264f8ea9f9989
Rayhan <rayhanprima99@gmail.com>
2022-07-17 15:59:14 +0700

    chore: add constants

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const REGISTRATION_LINK =
+  "https://gdg.community.dev/events/details/google-gdg-jakarta-presents-google-io-extended-jakarta-2022/";
+const IO_OFFICIAL_SITE_LINK = "https://io.google/2022/";
+
+export const CONSTANTS = { REGISTRATION_LINK, IO_OFFICIAL_SITE_LINK };
