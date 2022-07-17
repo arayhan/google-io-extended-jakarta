71c84e81f71af3d60a334eaa1b97a532b669bb80
Rayhan <rayhanprima99@gmail.com>
2022-07-17 21:03:52 +0700

    chore: add rest props to ButtonLink

--- src/components/atoms/Button/ButtonLink.jsx
+++ src/components/atoms/Button/ButtonLink.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Button.css";
 
-export function ButtonLink({ className, href, text, icon }) {
+export function ButtonLink({ className, href, text, icon, ...props }) {
   return (
     <a
       className={`button ${className}`}
       href={href}
       target="_blank"
       rel="noreferrer"
+      {...props}
     >
       <span className="tracking-wider">{text}</span>
       {icon && <span>{icon}</span>}
