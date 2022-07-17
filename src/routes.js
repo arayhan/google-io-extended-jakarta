c5782464c2e32ddd525378bda079e5e739992366
Rayhan <rayhanprima99@gmail.com>
2022-07-17 17:51:49 +0700

    chore: remove home from header routes

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,21 @@
 export const ROUTES = {
-	HOME: "/",
-	SPEAKERS: "/speakers",
-	SCHEDULES: "/schedules",
-	ORGANIZERS: "/organizers",
+  HOME: "/",
+  SPEAKERS: "/speakers",
+  SCHEDULES: "/schedules",
+  ORGANIZERS: "/organizers",
 };
 
 export const HEADER_ROUTES = [
-	{
-		path: ROUTES.HOME,
-		title: "Home",
-	},
-	{
-		path: ROUTES.SPEAKERS,
-		title: "Speakers",
-	},
-	{
-		path: ROUTES.SCHEDULES,
-		title: "Schedules",
-	},
-	{
-		path: ROUTES.ORGANIZERS,
-		title: "Organizers",
-	},
+  {
+    path: ROUTES.SPEAKERS,
+    title: "Speakers",
+  },
+  {
+    path: ROUTES.SCHEDULES,
+    title: "Schedules",
+  },
+  {
+    path: ROUTES.ORGANIZERS,
+    title: "Organizers",
+  },
 ];
